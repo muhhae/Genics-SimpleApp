c697c1a2ac65d4c921c10404175d052a9ea5727b
Muhhae <haekal.muhamat@gmail.com>
2023-06-21 19:44:59 +0700

    Simple calculator

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var input = document.getElementById("calc");
 var res = document.getElementById("res");
 var enter = document.getElementById("enter");
+var del = document.getElementById("del");
+var ac = document.getElementById("ac");
+
 
 const op = document.getElementsByClassName("kal");
 Array.from(op).forEach(e => {
@@ -11,6 +14,14 @@ Array.from(op).forEach(e => {
 
 enter.addEventListener("click", function () {
     res.textContent = eval(input.value);
+})
+
+del.addEventListener("click", function () {
+    input.value = input.value.slice(0, -1);
+})
+
+ac.addEventListener("click", function () {
+    input.value = "";
 })
 
 
